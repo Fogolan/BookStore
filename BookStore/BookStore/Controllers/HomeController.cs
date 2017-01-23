a40ef3080976ad57b19a068be010d0c5c321220e
Fogolan <Fogolan@gmail.ru>
2017-01-23 16:03:49 +0300

    23.01.2017

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BookStore.Models;
+using BookStore.Util;
 
 namespace BookStore.Controllers
 {
@@ -32,6 +33,32 @@ namespace BookStore.Controllers
             db.Purchases.Add(purchase);
             db.SaveChanges();
             return "Спасибо, " + purchase.Person + ", за покупку!";
+        }
+        [HttpGet]
+        public string Square(int a=10, int h=3)
+        {
+            double s = a * h / 2;
+            return "<h2>Площадь треугольника с основанием " + a +
+            " и высотой " + h + " равна " + s + "</h2>";
+        }
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир</h2>");
+        }
+        public ActionResult GetImage()
+        {
+            string path = "../Images/Emperor.jpg";
+            return new ImageResult(path);
+        }
+        public string UserInformation()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
         }
     }
 }
